a9a87744816e939dfbad5f9c3524332c5e5cdeb7
dashllh@qq.com <dashllh@qq.com>
2022-11-10 11:23:50 +0800

    完善失重率自动计算及显示

--- ISO11820-2020/wwwroot/views/view-report/view-report.js
+++ ISO11820-2020/wwwroot/views/view-report/view-report.js
@@ -50,24 +50,44 @@ class ReportView extends HTMLElement {
     loadTestDetails(data) {
         //清空现有试验明细
         this.clearDetails();
+        //查无记录的情况
+        if (data.length === 0) {
+            document.getElementById("txtProductName").value = "";
+            return;
+        }
+        //更新样品名称显示
+        document.getElementById("txtProductName").value = data[0].productname;
         //添加新明细数据并更新缓存
         data.forEach((detail, index) => {
+            //添加一行试验明细数据
             this.appendNewDetail(detail, index);
-            //添加文本框change事件监听器,以自动更新失重率
+            //对已添加的一行数据,注册文本框change事件监听器,以自动计算失重率
             let item = document.getElementById(`txtPostWeight${index}`);
+            //监听器方法在页面加载完成后执行
             item.addEventListener('change', (event) => {
-                let idx = item.closest('tr').rowIndex - 1; //此处tr索引为其在table中的索引,即包含标题行索引,减去1转换为tbody一致的索引
-                //验证用户输入
+                //此处tr索引为其在table中的索引,即包含标题行索引,减去1转换为tbody一致的索引
+                let idx = item.closest('tr').rowIndex - 1; 
+                //使用正则表达式验证残余质量
                 if (!this.#reg_float.test(event.target.value)) {
                     event.target.focus();
                     return;
                 } else {
                     this.#CurrentDetails[idx].postweight = parseFloat(event.target.value);
                 }
-                this.#tblTestDetail.rows[idx].cells[11].textContent =
-                    ((this.#CurrentDetails[idx].preweight - this.#CurrentDetails[idx].postweight) / this.#CurrentDetails[idx].preweight * 100).toFixed(1);                
+                //计算并更新对应行的失重率显示(四舍五入至小数点后1位)
+                let lostper = ((this.#CurrentDetails[idx].preweight - this.#CurrentDetails[idx].postweight) / this.#CurrentDetails[idx].preweight * 100).toFixed(1);                
+                let cell = this.#tblTestDetail.rows[idx].cells[11];
+                if (parseInt(lostper * 10) <= 500) { //达标
+                    cell.classList.remove("non-fullfilled");
+                    cell.classList.add("fullfilled");
+                } else { //不达标
+                    cell.classList.remove("fullfilled");
+                    cell.classList.add("non-fullfilled");
+                }
+                cell.textContent = lostper;  
             });
         });
+        //更新试验数据明细缓存
         this.#CurrentDetails = data;
     }
 
@@ -101,8 +121,26 @@ class ReportView extends HTMLElement {
         Cell8.textContent = data.finaltf2;
         Cell9.textContent = data.deltatf1;
         Cell10.textContent = data.deltatf2;
+        //根据火焰持续时间设置显示属性
         Cell11.textContent = data.flameduration;
-        Cell12.textContent = ((data.preweight - data.postweight) / data.preweight * 100).toFixed(1);
+        if (data.flameduration === 0) { //达标            
+            Cell11.classList.remove("non-fullfilled");
+            Cell11.classList.add("fullfilled");
+        } else { 
+            Cell11.classList.remove("fullfilled");
+            Cell11.classList.add("non-fullfilled");
+        }
+        //根据失重率设置显示属性
+        let lostper = ((data.preweight - data.postweight) / data.preweight * 100).toFixed(1);    
+        let tmp = parseInt(lostper * 10);
+        if (tmp <= 500 && tmp >= 0) { //达标
+            Cell12.classList.remove("non-fullfilled");
+            Cell12.classList.add("fullfilled");
+        } else { //不达标
+            Cell12.classList.remove("fullfilled");
+            Cell12.classList.add("non-fullfilled");
+        }
+        Cell12.textContent = lostper; 
     }
 
     /* 清空当前明细(保留首行) */
@@ -122,7 +160,7 @@ class ReportView extends HTMLElement {
                 <div>
                     <label for="txtProductId">样品编号:</label>
                     <input type="text" name="txtProductId" id="txtProductId">
-                    <button id="btnSearch">检索</button>
+                    <button id="btnSearch" class="cmdbutton">检索</button>
                 </div>
                 <div>
                     <label for="txtProductName">样品名称:</label>
@@ -155,7 +193,7 @@ class ReportView extends HTMLElement {
             </div>
             <!-- 操作按钮 -->
             <div class="action">
-                <button id="btnGenerateFinalReport">生成汇总报告</button>
+                <button id="btnGenerateFinalReport" class="cmdbutton">生成汇总报告</button>
             </div>
         `;
     }
